39bcc6987f8e69174b8e2f0d328a9117a86b46b3
Saeed Khatami <0xb4dc0d3x@gmail.com>
2023-10-10 11:07:16 +0330

    empty_fn removed

--- Header/workload.hpp
+++ Header/workload.hpp
@@ -12,6 +12,9 @@ void workload5();
 void workload6();
 void workload7();
 void workload8();
+void workload_selector(int amount_of_threads_available);
+
+/*
 void workload9();
 void workload10();
 void workload11();
@@ -20,4 +23,20 @@ void workload13();
 void workload14();
 void workload15();
 void workload16();
-void workload_selector(int amount_of_threads_available);
+void workload17();
+void workload18();       in here you can add your workloads, released this as a poc and ran it on 8 cores..
+void workload19();
+void workload20();
+void workload21();
+void workload22();
+void workload23();
+void workload24();
+void workload25();
+void workload26();
+void workload27();
+void workload28();
+void workload29();
+void workload30();
+void workload31();
+void workload32();
+*/
--- Source/workload.cpp
+++ Source/workload.cpp
@@ -32,38 +32,6 @@ void workload8() {
 	cout << "Workload 8" << endl;
 }
 
-void workload9() {
-	cout << "Workload 9" << endl;
-}
-
-void workload10() {
-	cout << "Workload 10" << endl;
-}
-
-void workload11() {
-	cout << "Workload 11" << endl;
-}
-
-void workload12() {
-	cout << "Workload 12" << endl;
-}
-
-void workload13() {
-	cout << "Workload 13" << endl;
-}
-
-void workload14() {
-	cout << "Workload 14" << endl;
-}
-
-void workload15() {
-	cout << "Workload 15" << endl;
-}
-
-void workload16() {
-	cout << "Workload 16" << endl;
-}
-
 void workload_selector(int index) {
 
 		switch (index) {
@@ -92,29 +60,6 @@ void workload_selector(int index) {
 		case 7:
 			workload8();
 			break;
-		case 8:
-			workload9();
-			break;
-		case 9:
-			workload10();
-			break;
-		case 10:
-			workload11();
-			break;
-		case 11:
-			workload12();
-			break;
-		case 12:
-			workload13();
-			break;
-		case 13:
-			workload14();
-			break;
-		case 14:
-			workload15();
-			break;
-		case 15:
-			workload16();
-			break;
+
 		}
 }
