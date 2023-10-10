c3cc26c033beaf076ec9b9c7288b1f90800b8be3
Saeed Khatami <0xb4dc0d3x@gmail.com>
2023-10-10 10:55:30 +0330

    Add project files.

---
+++ workload.cpp
@@ -0,0 +1,120 @@
+#include "workload.hpp"
+
+void workload1() {
+	cout << "Workload 1" << endl;
+}
+
+void workload2() {
+	cout << "Workload 2" << endl;
+}
+
+void workload3() {
+	cout << "Workload 3" << endl;
+}
+
+void workload4() {
+	cout << "Workload 4" << endl;
+}
+
+void workload5() {
+	cout << "Workload 5" << endl;
+}
+
+void workload6() {
+	cout << "Workload 6" << endl;
+}
+
+void workload7() {
+	cout << "Workload 7" << endl;
+}
+
+void workload8() {
+	cout << "Workload 8" << endl;
+}
+
+void workload9() {
+	cout << "Workload 9" << endl;
+}
+
+void workload10() {
+	cout << "Workload 10" << endl;
+}
+
+void workload11() {
+	cout << "Workload 11" << endl;
+}
+
+void workload12() {
+	cout << "Workload 12" << endl;
+}
+
+void workload13() {
+	cout << "Workload 13" << endl;
+}
+
+void workload14() {
+	cout << "Workload 14" << endl;
+}
+
+void workload15() {
+	cout << "Workload 15" << endl;
+}
+
+void workload16() {
+	cout << "Workload 16" << endl;
+}
+
+void workload_selector(int index) {
+
+		switch (index) {
+
+		case 0:
+			workload1();
+			break;
+		case 1:
+			workload2();
+			break;
+		case 2:
+			workload3();
+			break;
+		case 3:
+			workload4();
+			break;
+		case 4:
+			workload5();
+			break;
+		case 5:
+			workload6();
+			break;
+		case 6:
+			workload7();
+			break;
+		case 7:
+			workload8();
+			break;
+		case 8:
+			workload9();
+			break;
+		case 9:
+			workload10();
+			break;
+		case 10:
+			workload11();
+			break;
+		case 11:
+			workload12();
+			break;
+		case 12:
+			workload13();
+			break;
+		case 13:
+			workload14();
+			break;
+		case 14:
+			workload15();
+			break;
+		case 15:
+			workload16();
+			break;
+		}
+}
---
+++ workload.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+
+void workload1();
+void workload2();
+void workload3();
+void workload4();
+void workload5();
+void workload6();
+void workload7();
+void workload8();
+void workload9();
+void workload10();
+void workload11();
+void workload12();
+void workload13();
+void workload14();
+void workload15();
+void workload16();
+void workload_selector(int amount_of_threads_available);
